18d70a339f0168d0711c88fb7d2af58eb5630b3a
Long-Soeng167 <generalpurpose167@gmail.com>
2025-07-21 11:47:48 +0700

    add google analytic

--- resources/js/pages/cam-active-two/components/social.tsx
+++ resources/js/pages/cam-active-two/components/social.tsx
@@ -7,7 +7,7 @@ const Social = () => {
     return (
         <div className="z-10 my-2 flex items-center justify-start gap-2 rounded-full text-2xl transition-all duration-300 *:transition-all *:duration-300">
             {links?.map((item) => (
-                <button className="bg-background cursor-pointer rounded-full p-1 shadow-lg hover:-translate-y-5 hover:scale-125 dark:bg-[#191818]">
+                <button key={item.id} className="bg-background cursor-pointer rounded-full p-1 shadow-lg hover:-translate-y-5 hover:scale-125 dark:bg-[#191818]">
                     <a href={item.link || '#'} target="_blank">
                         <div className="flex size-10 shrink-0 justify-center rounded-lg md:justify-start">
                             <img src={`/assets/images/links/thumb/${item.image}`} className="mb-2 size-10 shrink-0 object-contain" alt="" />
